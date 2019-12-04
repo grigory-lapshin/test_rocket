3e2adb094773382775e59c6159869cf862d328cc
grigory-lapshin <30339972+sadsaviour@users.noreply.github.com>
2019-12-04 16:01:11 +0300

    charm: eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,31 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  "env": {
+      "node": true,
+      "es6": true
+  },
+  "extends": [
+      "airbnb", "prettier"
+  ],
+  "globals": {
+      "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react",  "prettier"
+  ],
+  "parser": "babel-eslint",
+  "rules": {
+          "prettier/prettier": "error", 
+          "react/jsx-filename-extension": ["off"], 
+          "react/prop-types": ["off"], 
+          "import/no-dynamic-require": ["off"] ,
+          "global-require": ["off"],
+  }
 };
