ea8a9fa35e3c0628319e9e2946ef5a976b9ec117
grigory-lapshin <30339972+sadsaviour@users.noreply.github.com>
2019-12-05 13:45:34 +0300

    feat: make scroll with snap points and springs

--- src/components/CardSelector.js
+++ src/components/CardSelector.js
@@ -7,31 +7,31 @@ import {useMemoOne} from 'use-memo-one';
 import {snapPoint, verticalPanGestureHandler} from 'react-native-redash';
 
 const {
-  add,
-  cond,
-  diff,
-  divide,
+  SpringUtils,
+  Value,
+  Clock,
   eq,
-  event,
-  exp,
-  lessThan,
-  and,
-  call,
-  block,
-  multiply,
-  pow,
+  startClock,
   set,
-  abs,
-  clockRunning,
+  add,
+  and,
   greaterOrEq,
   lessOrEq,
-  sqrt,
-  startClock,
-  stopClock,
+  cond,
+  decay,
+  block,
+  not,
+  spring,
+  abs,
+  multiply,
+  divide,
   sub,
-  Clock,
-  Value,
   useCode,
+  call,
+  neq,
+  diff,
+  pow,
+  min,
 } = Animated;
 
 const styles = StyleSheet.create({
@@ -46,9 +46,25 @@ const styles = StyleSheet.create({
   },
 });
 
+const scrollSpringConfig = {
+  ...SpringUtils.makeDefaultConfig(),
+  damping: new Value(50),
+  toValue: new Value(0),
+};
+
+const {height} = Dimensions.get('window');
+
+const friction = ratio => multiply(0.52, pow(sub(1, ratio), 2));
+
+const UPPER_BOUND = 0;
+const LOWER_BOUND = -1 * height * 0.33;
+
 const withScroll = ({translationY, velocityY, state: gestureState}) => {
   const clock = new Clock();
-  const offset = new Value();
+  const delta = new Value(0);
+  const isSpringing = new Value(0);
+
+  //   const isSpringing = new Value(0);
   const state = {
     time: new Value(0),
     position: new Value(0),
@@ -56,11 +72,74 @@ const withScroll = ({translationY, velocityY, state: gestureState}) => {
     finished: new Value(0),
   };
 
+  // I need to check bounds to restrict srolling
+  const isInBound = and(
+    lessOrEq(state.position, UPPER_BOUND),
+    greaterOrEq(state.position, LOWER_BOUND),
+  );
+
+  const overscroll = sub(
+    state.position,
+    cond(greaterOrEq(state.position, 0), UPPER_BOUND, LOWER_BOUND),
+  );
+
   return block([
+    startClock(clock),
+    set(delta, diff(translationY)),
+
     cond(
       eq(gestureState, State.ACTIVE),
-      [set(state.position, add(offset, translationY))],
-      [set(offset, state.position)],
+      // When dragging
+      [
+        set(isSpringing, 0),
+        set(
+          state.position,
+          add(
+            state.position,
+            cond(
+              isInBound,
+              delta,
+              multiply(
+                delta,
+                friction(min(divide(abs(overscroll), height), 1)),
+              ),
+            ),
+          ),
+        ),
+        set(state.velocity, velocityY),
+        set(state.time, 0),
+      ],
+      // When dragging is over
+      [
+        set(translationY, 0),
+        cond(
+          and(isInBound, not(isSpringing)),
+          // decay make scroll to have inertia
+          [
+            decay(clock, state, {deceleration: 0.997}),
+            set(isSpringing, 1),
+            set(
+              scrollSpringConfig.toValue,
+              snapPoint(state.position, state.velocity, [
+                LOWER_BOUND,
+                UPPER_BOUND,
+              ]),
+            ),
+            spring(clock, state, scrollSpringConfig),
+          ],
+          [
+            set(isSpringing, 1),
+            set(
+              scrollSpringConfig.toValue,
+              snapPoint(state.position, state.velocity, [
+                LOWER_BOUND,
+                UPPER_BOUND,
+              ]),
+            ),
+            spring(clock, state, scrollSpringConfig),
+          ],
+        ),
+      ],
     ),
     state.position,
   ]);
